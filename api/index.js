2b17182905d37912152a6a34f5998b4549245a01
hoang-trong-tan <esttan2018@gmail.com>
2023-07-17 12:40:35 +0700

    test

---
+++ api/index.js
@@ -0,0 +1,8 @@
+import { Comics } from "comics-api"
+
+const { Comics } = require("comics-api");
+
+// Sử dụng các phương thức từ gói `comics-api`
+Comics.getChapters('tham-tu-conan');
+Comics.getComments('tham-tu-conan');
+// và các phương thức khác theo hướng dẫn
